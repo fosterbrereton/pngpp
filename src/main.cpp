e01ffbcf23fc94b2b3ac47038a17fa7341fdf34b
Foster Brereton <fbrereto@adobe.com>
2017-04-27 10:31:43 -0700

    getting set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,42 @@
-/******************************************************************************/
-// Boilerplate.
-/******************************************************************************/
+/**************************************************************************************************/
+// PNGpp copyright 2017 Foster Brereton. See LICENSE.txt for license details.
+/**************************************************************************************************/
 
 // stdc++
 #include <iostream>
 
 // tbb
-// #include <tbb/mutex.h>
+#include <tbb/parallel_for_each.h>
 
 // boost
-// #include <boost/program_options.hpp>
+#include <boost/program_options.hpp>
+
+// libpng
+#include <png.h>
 
 // application
-// #include "header.hpp"
+#include <pngpp/files.hpp>
 
-/******************************************************************************/
+/**************************************************************************************************/
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, world!\n";
+using namespace pngpp;
+
+/**************************************************************************************************/
+
+int main(int argc, char** argv) try {
+    if (argc <= 1)
+        throw std::runtime_error("Source file not specified");
+
+    path_t path(canonical(argv[1]));
+
+    std::cout << "Found `" << path.string() << "`\n";
+
+    return 0;
+} catch (const std::exception& error) {
+    std::cerr << "Fatal error: " << error.what() << '\n';
+    return 0;
+} catch (...) {
+    std::cerr << "Fatal error: unknown\n";
+    return 0;
 }
-
-/******************************************************************************/
+/**************************************************************************************************/
